e439a59c570e52f6d8e66c004f97191e678d3422
desmond kang <dkang.kky@gmail.com>
2016-08-29 18:22:03 +0800

    Basic Done

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,27 @@
+enable :sessions
+
 get '/' do
+  @all = Url.all
+  @errors = session[:error]
+  session[:error] = nil
   erb :"static/index"
 end
+
+post '/urls' do
+	# byebug
+	@url = Url.new(params[:url])
+	# byebug
+	@url.short_url = @url.shorten 
+	# byebug
+	if @url.save
+		redirect '/'
+	else
+		session[:error] = @url.errors.messages[:long_url][0]
+		redirect '/'
+	end
+end
+
+get '/:short_url' do
+	@select = Url.find_by(short_url: params[:short_url])
+	redirect @select.long_url
+end 
